d352ab7e4609271f6a1747bbd7f6b168c442f63f
CAMM961001 <miguel.castama@gmail.com>
2023-06-19 12:29:48 -0600

    Inicializar página de visualizaciones y fijar ancho de barra

---
+++ pages/2_Visualizaciones.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+# Ruta absoluta de directorio actual
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Ruta absoluta del directorio de módulos
+SRC_PATH = os.path.join(DIR_PATH, '..', 'src')
+
+# Agregar ruta de módulo al path
+sys.path.append(SRC_PATH)
+
+
+# Carga de librerías
+import streamlit as st
+
+from ajustes import Ajustes
+
+
+# ----------------------------------------------------------- AJUSTES GENERALES
+
+
+ajustes = Ajustes()
+
+
+# Site settings
+st.set_page_config(
+    page_title = ajustes.config['app']['paginas'][0]
+    ,layout = 'wide'
+    ,initial_sidebar_state = 'expanded')
+
+
+# --------------------------------------------------------------- BARRA LATERAL
+
+
+ajustes.fijar_ancho_barra_lateral()
