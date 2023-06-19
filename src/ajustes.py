1522498449df1b77ec6d602aa46004ea17ef30f6
CAMM961001 <miguel.castama@gmail.com>
2023-06-19 17:39:53 -0600

    Docstring de función para almacenar archivo

--- src/ajustes.py
+++ src/ajustes.py
@@ -6,27 +6,58 @@ from streamlit import write
 from datetime import datetime
 
 
-def guardar_archivo(path:str):
+def guardar_archivo(contenido:str, path:str, formato = 'txt'):
+    """
+    Esta función es para almacenar el contenido de un archivo
+    en una ruta dada, generando a su vez un respaldo de archivos previos con
+    el mismo nombre en caso de existir en un directorio de históricos.
+
+    Parámetros:
+    - contenido: Contenido del archivo que se almacenará
+    - path: Ruta en la que se almacenará
+    - formato: Extensión del archivo
+    """
+
+    # Extraer directorio del archivo
+    dir = os.path.dirname(path)
+
+    # Extraer nombre original del archivo
+    filename = os.path.basename(path)
+
     # Respaldar archivo anterior si existe
     if os.path.isfile(path = path):
-        # Extraer nombre original del archivo
-        dst = path.partition('.txt')[0]
-        
+
         # Agregar sufijo de fecha de respaldo
-        dst += f"_{datetime.now().strftime('%Y%m%d%-H%M%S')}.txt"
+        newname = filename.partition(f'.{formato}')[0]
+        newname += f"_{datetime.now().strftime('%Y%m%d-%H%M%S')}.{formato}"
+
+        # Archivo de destino
+        dst = os.path.join(dir, 'historico', newname)
         
-        # Mover archivo de carpeta de históricos
+        # Mover archivo a carpeta de históricos
         shutil.move(src=path, dst=dst)
 
-    prompt = f'Archivo almacenado en la ruta: <br><b>{path}</b>'
-    write(prompt, unsafe_allow_html = True)
+        # Mensaje de confirmación
+        write(
+            f'Se respaldó versión previa con el nombre: <b>{newname}</b>'
+            ,unsafe_allow_html = True)
+    
+    # Escribir nuevo archivo
+    with open(file = path, mode = 'w') as file:
+        file.write(contenido)
+    file.close()
+
+    # Mensaje de confirmación
+    write(
+        f'Nuevo archivo almacenado en la ruta: <br><b>{path}</b>'
+        ,unsafe_allow_html = True)
 
 
 class Ajustes:
 
-
     def __init__(self):
         """Initialize the app's static settings"""
+
         # Relative path to directory root
         self.ROOT = os.path.dirname(os.path.dirname(__file__))
         
