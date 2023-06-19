ebf47356571ab6ea9c133828a66282e2e5e40389
CAMM961001 <miguel.castama@gmail.com>
2023-06-19 14:41:31 -0600

    Caso en el que no existe arvhivo de criterios

--- pages/3_Consultas.py
+++ pages/3_Consultas.py
@@ -1,9 +1,9 @@
 # ---------------------------------------------------------- CARGA DE LIBRERÍAS
-
-
 import os
 import sys
 import streamlit as st
+
+from io import StringIO
 
 # Ruta absoluta de directorio actual
 DIR_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -18,10 +18,7 @@ from ajustes import Ajustes
 
 
 # ----------------------------------------------------------- AJUSTES GENERALES
-
-
 ajustes = Ajustes()
-
 
 # Site settings
 st.set_page_config(
@@ -29,10 +26,11 @@ st.set_page_config(
     ,layout = 'wide'
     ,initial_sidebar_state = 'expanded')
 
+# Título de la pagina
+st.title(body = 'Consultas')
+
 
 # --------------------------------------------------------------- BARRA LATERAL
-
-
 ajustes.fijar_ancho_barra_lateral()
 
 with st.sidebar:
@@ -51,20 +49,50 @@ with st.sidebar:
         </p>'''
         ,unsafe_allow_html = True)
 
-    # Acciones de la página
-    archivo_criterio_busqueda = st.file_uploader(
+    # Carga de archivo de criterios de búsqueda
+    nuevos_criterios_busqueda = st.file_uploader(
         label = 'Carga de archivo de criterios de búsqueda:'
         ,type = 'txt'
         ,accept_multiple_files = False
-        ,help = '''Carga aquí tu archivo de criterios de búsqueda cuando existan
-        modificaciones a la información desplegada actualmente''')
-    
+        ,help = '''Carga aquí tu archivo de criterios de búsqueda cuando
+        existan modificaciones a la información desplegada actualmente''')
+
+    # ----------------------------------------------- CARGA DE NUEVOS CRITERIOS
+
+    # Acciones si existe nuevo archivo de criterios de búsqueda
+    if nuevos_criterios_busqueda is not None:
+        
+        # Leer archivo cargado como cadena de caracteres
+        __archivo__ = StringIO(initial_value = (
+            nuevos_criterios_busqueda
+            .getvalue()
+            .decode("utf-8")))
+        
+        # Generar lista de nuevos criterios
+        __criterios__ = __archivo__.read().splitlines()
+
+        # Respaldar archivo anterior
 
 
+# -------------------------------------------- LECTURA DE CRITERIOS DE BÚSQUEDA
+CRITERIOS = os.path.join(
+    ajustes.ROOT
+    ,ajustes.config['datos']['dirname']
+    ,ajustes.config['datos']['criterios']['dirname']
+    ,ajustes.config['datos']['criterios']['workfile'])
 
-# ----------------------------------------------------------- CARGA DE ARCHIVOS
+try:
+    with open(file = CRITERIOS, mode='r') as file:
+        CRITERIOS = file.read()
+    file.close()
 
+    print(CRITERIOS)
 
-if __name__ == '__main__':
-    print(archivo_criterio_busqueda)
-    print(type(archivo_criterio_busqueda))
+except FileNotFoundError:
+    st.write(f'''
+        <br>No existe el archivo
+        <b>{ajustes.config['datos']['criterios']['workfile']}</b>
+        en la ruta: <b>{CRITERIOS}</b></br>
+        <br>Utiliza la barra lateral para cargar un archivo de criterios de
+        búsqueda.</br>'''
+        ,unsafe_allow_html=True)
